6db0ba6e2cb620ce14ddfcabf6df328bf0b8e86d
nambv <nambuivu@gmail.com>
2018-10-03 18:23:22 +0700

    Init project

---
+++ app/src/main/java/com/nambv/android_stackoverflow/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nambv.android_stackoverflow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
